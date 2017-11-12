d74dd217ff905123bbc94ab155e5defec2d967ba
Sam E <sje74@nau.edu>
2017-11-12 12:26:22 -0700

    sam

---
+++ app/src/main/java/com/example/nathan_mccauslin/ibuy/ItemListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.nathan_mccauslin.ibuy;
+
+/**
+ * Created by kagom on 11/7/2017.
+ */
+
+public class ItemListAdapter {
+}
--- app/src/main/java/com/example/nathan_mccauslin/ibuy/PurchaseAnalytics.java
+++ app/src/main/java/com/example/nathan_mccauslin/ibuy/PurchaseAnalytics.java
@@ -13,6 +13,8 @@ public class PurchaseAnalytics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_analytics);
+        // hi
+        System.out.println("hola");
     }
 
 }
